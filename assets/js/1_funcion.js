3759e9e119548bb126af9ad3ca30ec1e682f717a
pblo <pdiazarenas541@gmail.com>
2025-07-07 00:37:46 +0100

    FEAT/ create nav + script.js + pintar.html

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,17 @@
+
+// función declaración a función de expresión:
+
+example = function(a,b,c){
+    return a + b + c;
+}
+
+
+console.log(` Declaración -> Función de expresión:
+    
+    example = function(a,b,c){
+    return a + b + c;
+    }
+ 
+    exampleResult(2,3,4) da ${example(2,3,4)}`);
+
+
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,13 @@
+// función a arrow function
+
+let suma = (a,b) => {
+    return a + b;
+}
+
+console.log(` Función  de Expresión -> Arrow Function:
+    
+    función:  let suma = (a,b) => {
+    return a + b;
+    }  
+
+    suma(2,3) da ${suma(2,3)}`);
