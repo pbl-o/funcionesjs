414368b8b937f969fb3040888823953765ae3fec
pblo <pdiazarenas541@gmail.com>
2025-07-07 01:43:47 +0100

    FEAT / add script.js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// toggle var;
+let state = false;
+let element = document.querySelector('#ele1');
+//intial look:
+element.style.backgroundColor = 'green';
+element.style.color = 'white';
+
+
+//función Cambio de color
+const pintarArea = (ele, color = 'green') => {
+    ele.style.backgroundColor = color;
+}
+
+
+//listener
+
+element.addEventListener('click', (e) => {
+    state = !state;
+    if(state){
+        pintarArea(e.target, 'yellow');
+    }else{
+        pintarArea(e.target);
+    }
+});
